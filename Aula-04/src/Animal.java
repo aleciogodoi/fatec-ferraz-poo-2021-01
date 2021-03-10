24806401e84d3fdb847e6247688fe5c913be3fe3
Alécio <alecio.godoi@ymail.com>
2021-03-10 09:46:50 -0300

    Aula 04 - Classe Animal Atualizada

--- Aula-04/src/Animal.java
+++ Aula-04/src/Animal.java
@@ -31,21 +31,23 @@ public class Animal {
 	}
 
 	public void comer() {
-		if(estado && caloria<100) {
+		if(estado && forca>=2 && caloria<100) {
 			caloria += 10;
 			forca -= 2;
 			JOptionPane.showMessageDialog(null, nome+" comeu!");
 		} else {
 			if (!estado) {
 				JOptionPane.showMessageDialog(null, nome+" não come! Está morto!");
-			} else  {
+			} else if (caloria > 100) {
 				JOptionPane.showMessageDialog(null, nome+" não come! Está cheio!");		
+			} else {
+				JOptionPane.showMessageDialog(null, nome+" não come! Está exausto!");	
 			}
 		}
 	}
 
 	public void correr() {
-		if(estado && caloria>=5) {
+		if(estado && caloria>=5 && forca>=5) {
 			caloria -= 5;
 			forca -= 5;
 			JOptionPane.showMessageDialog(null, nome+" correu!");
@@ -58,4 +60,28 @@ public class Animal {
 		}
 	}
 	
+	public void dormir() {
+		if(estado && caloria>=2 && forca<100) {
+			caloria -= 2;
+			forca += 10;
+			JOptionPane.showMessageDialog(null, nome+" dormiu!");
+		} else {
+			if (!estado) {
+				JOptionPane.showMessageDialog(null, nome+" não dorme! Está morto!");	
+			} else {
+				JOptionPane.showMessageDialog(null, nome+" não dorme! Está sem calorias!");	
+			}
+		}
+	}
+	
+	public void aniversario() {
+		idade++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Animal [nome=" + nome + ", idade=" + idade + ", caloria=" + caloria + ", forca=" + forca + ", estado="
+				+ estado + "]";
+	}
+
 }
--- Aula-04/src/TestarAnimal.java
+++ Aula-04/src/TestarAnimal.java
@@ -10,20 +10,25 @@ public class TestarAnimal {
 					+ "1 - Comer\n"
 					+ "2 - Correr\n"
 					+ "3 - Dormir\n"
-					+ "4 - Morrer";	
+					+ "4 - Aniversário\n"
+					+ "5 - Morrer";	
 		do {
+			System.out.println(a1);
 			opcao = Integer.valueOf(JOptionPane.showInputDialog(menu));
 			switch (opcao) {
 			case 1:
 				a1.comer();
 				break;
 			case 2:
-				// correr
+				a1.correr();
 				break;
 			case 3:
-				// dormir
+				a1.dormir();
 				break;
 			case 4:
+				a1.aniversario();
+				break;
+			case 5:
 				a1.morrer();
 				break;
 			}
