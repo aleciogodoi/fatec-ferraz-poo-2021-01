3619f645d0d07640f87ec095ea895d6c4ab8b893
Alécio <alecio.godoi@ymail.com>
2021-05-12 13:01:47 -0300

    Aula 14 - Exemplo Conexão SQLite

--- Aula-14/src/ExemploBD_01/Teste.java
+++ Aula-14/src/ExemploBD_01/Teste.java
@@ -1,6 +1,9 @@
 package ExemploBD_01;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Teste {
 	public static void main(String[] args) {
@@ -9,6 +12,25 @@ public class Teste {
 		Conexao conexao = new Conexao();
 		Connection conn;
 		conn = conexao.conectar();
+		
+		// String como comando SQL
+		String sql = "Select * From Empregado";
+		System.out.println(sql);
+		
+		try {
+			// Criação de objeto para execução dos comandos SQL
+			Statement comandoSQL = conn.createStatement();
+			ResultSet rs = comandoSQL.executeQuery(sql);   // Execução do SQL 
+			while (rs.next()) {
+				System.out.print(rs.getString("idEmpregado")+"\t");
+				System.out.print(rs.getString("NomeEmpregado")+"\t\t");
+				System.out.print(rs.getString("Cargo")+"\t\t");
+				System.out.println(rs.getString("Salario"));
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Erro de execução comando\n"+e);
+		}
 		
 		// Fechando a conexão
 		conexao.fechar(conn);
