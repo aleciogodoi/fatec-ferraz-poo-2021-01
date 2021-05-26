f8169ffabefbf89d3c58f1718f150ca811475e10
Alécio <alecio.godoi@ymail.com>
2021-05-26 12:01:54 -0300

    Aula 16 - Exemplo - DAO

--- Aula-16/src/ExemploDAO/TesteUsuario.java
+++ Aula-16/src/ExemploDAO/TesteUsuario.java
@@ -3,7 +3,16 @@ package ExemploDAO;
 public class TesteUsuario {
 
 	public static void main(String[] args) {
+		UsuarioDAO.insert(new Usuario(1,"Pedro","pedro@gmail.com","11 96656-0001","Rua das Couves, 125 - Centro"));
+		UsuarioDAO.insert(new Usuario(2,"Paula","paula@gmail.com","11 98887-0001","Rua das Couves, 100 - Centro"));
+		UsuarioDAO.insert(new Usuario(3,"Guilherme","guilherme@gmail.com","11 9888-0002","Rua das Couves, 200 - Centro"));
 		System.out.println(UsuarioDAO.find(1));
-	}
 
+		for (Usuario usuario: UsuarioDAO.find("P%")) {
+			System.out.println(usuario);
+		}
+		for (Usuario usuario: UsuarioDAO.getAll()) {
+			System.out.println(usuario);
+		}
+	}
 }
--- Aula-16/src/ExemploDAO/UsuarioDAO.java
+++ Aula-16/src/ExemploDAO/UsuarioDAO.java
@@ -10,9 +10,26 @@ import Exemplos.Conexao;
 
 public class UsuarioDAO {
 	private static Connection conn;
-	private static Conexao conexao; 
+	private static Conexao conexao;
 
 	public static void insert(Usuario usuario) {
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Insert Into Usuario (idUsuario, Nome, Email, Telefone, Endereco) "
+				+  "Values              (        ?,    ?,     ?,        ?,        ?)";
+		
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			comandoSQL.setInt(1, usuario.getIdUsuario());
+			comandoSQL.setString(2, usuario.getNome());
+			comandoSQL.setString(3, usuario.getEmail());
+			comandoSQL.setString(4, usuario.getTelefone());
+			comandoSQL.setString(5, usuario.getEndereco());
+			comandoSQL.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void update(Usuario usario) {
@@ -40,7 +57,7 @@ public class UsuarioDAO {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} 
+		}
 		conexao.fechar(conn);
 		return usuario;
 	}
@@ -49,7 +66,7 @@ public class UsuarioDAO {
 		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 		conexao = new Conexao();
 		conn = conexao.conectar();
-		String sql = "Select * From Usuario Where Nome = ?";
+		String sql = "Select * From Usuario Where Nome like ?";
 
 		try {
 			PreparedStatement comandoSQL = conn.prepareStatement(sql);
@@ -62,15 +79,37 @@ public class UsuarioDAO {
 				usuario.setEmail(rsDados.getString("Email"));
 				usuario.setTelefone(rsDados.getString("Telefone"));
 				usuario.setEndereco(rsDados.getString("Endereco"));
+				usuarios.add(usuario);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} 
+		}
 		conexao.fechar(conn);
 		return usuarios;
 	}
 
 	public static ArrayList<Usuario> getAll() {
-		return null;
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql = "Select * From Usuario";
+
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			ResultSet rsDados = comandoSQL.executeQuery();
+			while (rsDados.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setIdUsuario(rsDados.getInt("idUsuario"));
+				usuario.setNome(rsDados.getString("Nome"));
+				usuario.setEmail(rsDados.getString("Email"));
+				usuario.setTelefone(rsDados.getString("Telefone"));
+				usuario.setEndereco(rsDados.getString("Endereco"));
+				usuarios.add(usuario);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
+		return usuarios;
 	}
 }
