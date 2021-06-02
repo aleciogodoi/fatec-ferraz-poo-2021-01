5842722ae99b8e387246c6bff68dfdd0230aa99a
Alécio <alecio.godoi@ymail.com>
2021-06-02 11:51:35 -0300

    Aula 17 - DAO - CRUD

--- Aula-17/src/PessoaDAO.java
+++ Aula-17/src/PessoaDAO.java
@@ -1,16 +1,54 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PessoaDAO {
-	private static void insert(Pessoa pessoa) {	
+	private static Connection conn;
+	private static Conexao conexao;
+	
+	public static void insert(Pessoa pessoa) {	
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Insert Into Pessoa (idPessoa, Nome, Peso, Altura, Idade) "
+				+ "Values (?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			comandoSQL.setInt(1, pessoa.getIdPessoa());
+			comandoSQL.setString(2, pessoa.getNome());
+			comandoSQL.setFloat(3, pessoa.getPeso());
+			comandoSQL.setFloat(4, pessoa.getAltura());
+			comandoSQL.setInt(5, pessoa.getIdade());
+			comandoSQL.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
-	private static void update(Pessoa pessoa) {	
+	
+	public static void update(Pessoa pessoa) {	
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Update Pessoa Set Nome=?, Peso=?, Altura=?, Idade=? Where idPessoa=?";
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			comandoSQL.setString(1, pessoa.getNome());
+			comandoSQL.setFloat(2, pessoa.getPeso());
+			comandoSQL.setFloat(3, pessoa.getAltura());
+			comandoSQL.setInt(4, pessoa.getIdade());
+			comandoSQL.setInt(5, pessoa.getIdPessoa());
+			comandoSQL.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
-	private static void delete(int idPessoa) {	
+	public static void delete(int idPessoa) {	
 	}
-	private static Pessoa find(int idPessoa) {
+	public static Pessoa find(int idPessoa) {
 		return null;
 	}
-	private static ArrayList<Pessoa> getAll(){
+	public static ArrayList<Pessoa> getAll(){
 		return null;
 	}
 }
---
+++ Aula-17/src/Teste.java
@@ -0,0 +1,11 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		//PessoaDAO.insert(new Pessoa(1,"Pedro", 71.5f, 1.75f, 20));
+		PessoaDAO.update(new Pessoa(1,"Pedro Alcantara", 71.5f, 1.75f, 21));
+		
+	}
+
+}
