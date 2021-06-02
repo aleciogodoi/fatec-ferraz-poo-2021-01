19c1cd7a3d9ee25bcc74fc23d990f6ba354f99b2
Alécio <alecio.godoi@ymail.com>
2021-06-02 12:14:55 -0300

    Aula 17 - DAO - CRUD

--- Aula-17/src/PessoaDAO.java
+++ Aula-17/src/PessoaDAO.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -43,12 +44,65 @@ public class PessoaDAO {
 		}
 		conexao.fechar(conn);
 	}
+
 	public static void delete(int idPessoa) {	
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Delete From Pessoa Where idPessoa=?";
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			comandoSQL.setInt(1, idPessoa);
+			comandoSQL.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
+	
 	public static Pessoa find(int idPessoa) {
-		return null;
+		Pessoa pessoa = new Pessoa();
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Select * From Pessoa Where idPessoa=?";
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			comandoSQL.setInt(1, idPessoa);
+			ResultSet rsDados = comandoSQL.executeQuery();
+			if (rsDados.next()) {
+				pessoa.setIdPessoa(rsDados.getInt("idPessoa"));
+				pessoa.setNome(rsDados.getString("Nome"));
+				pessoa.setPeso(rsDados.getFloat("Peso"));
+				pessoa.setAltura(rsDados.getFloat("Altura"));
+				pessoa.setIdade(rsDados.getInt("Idade"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
+		return pessoa;
 	}
+
 	public static ArrayList<Pessoa> getAll(){
-		return null;
+		ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+		conexao = new Conexao();
+		conn = conexao.conectar();
+		String sql="Select * From Pessoa";
+		try {
+			PreparedStatement comandoSQL = conn.prepareStatement(sql);
+			ResultSet rsDados = comandoSQL.executeQuery();
+			while (rsDados.next()) {
+				Pessoa pessoa = new Pessoa();
+				pessoa.setIdPessoa(rsDados.getInt("idPessoa"));
+				pessoa.setNome(rsDados.getString("Nome"));
+				pessoa.setPeso(rsDados.getFloat("Peso"));
+				pessoa.setAltura(rsDados.getFloat("Altura"));
+				pessoa.setIdade(rsDados.getInt("Idade"));
+				pessoas.add(pessoa);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
+		return pessoas;
 	}
 }
--- Aula-17/src/Teste.java
+++ Aula-17/src/Teste.java
@@ -1,11 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class Teste {
 
 	public static void main(String[] args) {
 
+		//PessoaDAO.insert(new Pessoa(2,"Maria", 67.5f, 1.70f, 21));
 		//PessoaDAO.insert(new Pessoa(1,"Pedro", 71.5f, 1.75f, 20));
-		PessoaDAO.update(new Pessoa(1,"Pedro Alcantara", 71.5f, 1.75f, 21));
-		
+		//PessoaDAO.update(new Pessoa(1,"Pedro Alcantara", 71.5f, 1.75f, 21));
+		//PessoaDAO.delete(1);
+		JOptionPane.showMessageDialog(null, PessoaDAO.find(1));
+		JOptionPane.showMessageDialog(null, PessoaDAO.getAll());
 	}
 
 }
